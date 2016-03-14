233fe3c52c1826b13ec46ca9aa8804b66c93fdb5
TylerA73 <tylera73@gmail.com>
2016-03-13 20:21:39 -0600

    Commit five
    Navbar now works exactly as intended
    Slides up and fades out on down scroll
    Slides down and fades in on up scroll
    Sliding up only happens when scroll is greater that 55 px from top of
    web page
    At the very top, navbar border vanishes
    Scroll down 1 px, border reappears
    Fixed issue where navbar is always transparent, but so is text. Now
    text is not transparent. (Will only work if browser supports CSS3.)
    Contact area aligned properly to centre

--- includes/functions.js
+++ includes/functions.js
@@ -1,14 +1,23 @@
 var didScroll;
 var lastScrollTop = 0;
 var delta = 5;
-var navbarHeight = $('#navbar').outerHeight();
-console.log(navbarHeight);
+var navbarHeight = $('#navbar').outerHeight(); 
 
-//$(document).ready(function(){
+$(document).ready(function(){
     
    $(window).scroll(function(event){
         
-        didScroll = true;
+        if ($(window).scrollTop() > 1) {
+            $('#navbar').removeClass('nav-no-border').addClass('nav-border');
+        } else {
+            $('#navbar').removeClass('nav-border').addClass('nav-no-border');
+        }
+        
+        if ($(window).scrollTop() > 55) {
+            didScroll = true;
+        } else {
+            didScroll = false;
+        }
         
     });
    
@@ -18,27 +27,27 @@ console.log(navbarHeight);
             didScroll = false;
         }
     
-    }, 50);
+    }, 0);
    
    function hasScrolled() {
         var st = $(this).scrollTop();
         
-        if (Math.abs(lastScrollTop-st) <= delta) {
+        if (Math.abs(lastScrollTop-st) <= delta) { 
             return;
         }
         
         if ((st > lastScrollTop) && (st > navbarHeight)) {
             
             $('#navbar').removeClass('nav-show').addClass('nav-hide');
-            console.log("hide");
+            //console.log("hide");
             
         } else {
             if ((st + $(window).height()) < ($(document).height())) {
                 $('#navbar').removeClass('nav-hide').addClass('nav-show');
-                console.log("show");
+                //console.log("show");
             }
         }
         
         lastScrollTop = st;
    }
-//});
+});
